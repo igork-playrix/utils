7a9ee5ecb9cdb3e51812a4069908d2970a1fc150
Игорь Кислицын <igor.kislitsin@playrix.com>
2022-02-03 15:26:51 +0700

    added printing sprite info

--- cut.py
+++ cut.py
@@ -1,30 +1,36 @@
 #!/usr/bin/env python3
 
-from sys import argv
+import sys
+import json
 from psd_tools import PSDImage
 
-def save_layer(layer, prefix=''):
+def save_layer(layer, info, prefix=''):
     if not layer.is_visible():
         return
 
     if not layer.is_group():
-        name = f"{prefix}{layer.name}.png"
-        print(f"Saving {name}...")
-        layer.topil().save(name)
+        name = f"{prefix}{layer.name}"
+
+        print(f"Saving {name}...", file=sys.stderr)
+        layer.topil().save(f'{name}.png')
+        info.append({'image': f'pack/{name}', 'position': {'x': layer.bbox[0], 'y': layer.bbox[1]}})
     else:
         for child in layer:
-            save_layer(child, f"{layer.name}-")
+            save_layer(child, info, f"{layer.name}-")
 
 def main():
-    if len(argv) < 2:
-        print("Usage: cut.py image.psd")
+    if len(sys.argv) < 2:
+        print("Usage: cut.py image.psd", file=sys.stderr)
         return 1
 
-    psd_name = argv[1]
+    psd_name = sys.argv[1]
     psd = PSDImage.open(psd_name)
 
+    info = []
     for layer in psd:
-        save_layer(layer)
+        save_layer(layer, info)
+
+    print(json.dumps(info, indent=4))
 
 if __name__ == '__main__':
     main()
